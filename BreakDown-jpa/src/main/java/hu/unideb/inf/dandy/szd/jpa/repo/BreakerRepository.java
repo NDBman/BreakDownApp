69469809fc07bfeb497466fcffc1b8db120e6aa0
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-05 02:42:01 +0100

    Cretated first repo.

---
+++ BreakDown-jpa/src/main/java/hu/unideb/inf/dandy/szd/jpa/repo/BreakerRepository.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.dandy.szd.jpa.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import hu.unideb.inf.dandy.szd.jpa.entity.Breaker;
+
+@Repository
+@Transactional
+public interface BreakerRepository extends JpaRepository<Breaker, Long>{
+
+}
