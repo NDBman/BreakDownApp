504b077d2a979ca70c45e1791379badc0667e657
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-06 14:03:48 +0100

    Deleted unused imports.

--- BreakDown-services/src/main/java/hu/unideb/inf/dandy/szd/services/config/BreakDownServiceModuleConfig.java
+++ BreakDown-services/src/main/java/hu/unideb/inf/dandy/szd/services/config/BreakDownServiceModuleConfig.java
@@ -2,9 +2,6 @@ package hu.unideb.inf.dandy.szd.services.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-import hu.unideb.inf.dandy.szd.jpa.config.BreakDownJpaModulConfig;
 
 @Configuration
 @ComponentScan(basePackages={"hu.unideb.inf.dandy.szd.services"})
