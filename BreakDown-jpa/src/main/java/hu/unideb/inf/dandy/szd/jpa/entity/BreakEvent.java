da605d1346fbc539963f7eb4dd1af35ee62d51e3
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-16 14:23:48 +0100

    Changed structure back due to error.

--- BreakDown-jpa/src/main/java/hu/unideb/inf/dandy/szd/jpa/entity/BreakEvent.java
+++ BreakDown-jpa/src/main/java/hu/unideb/inf/dandy/szd/jpa/entity/BreakEvent.java
@@ -3,7 +3,9 @@ package hu.unideb.inf.dandy.szd.jpa.entity;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,8 +13,11 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class BreakEvent extends SimpleEvent {
+public class BreakEvent extends AbstractEvent {
 
 	@ManyToMany(mappedBy="winnedEvents")
 	private List<Breaker> winners;
+	@ManyToOne
+	@JoinColumn(name="competition", referencedColumnName="id")
+	private Competition competition;
 }
