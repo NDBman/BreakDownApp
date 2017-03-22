6ead48a5cc66b27333e347626fcc8ad98b3b7711
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-22 22:15:34 +0100

    Created Role repository.

---
+++ BreakDown-jpa/src/main/java/hu/unideb/inf/dandy/szd/jpa/repo/RoleRepository.java
@@ -0,0 +1,9 @@
+package hu.unideb.inf.dandy.szd.jpa.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hu.unideb.inf.dandy.szd.jpa.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
