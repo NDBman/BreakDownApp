c4d3d34bd1064610da557ff1d163e867f910aedd
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-22 22:09:42 +0100

    Renamed Ranked class to Role.

--- BreakDown-jpa/src/main/java/hu/unideb/inf/dandy/szd/jpa/entity/Rank.java
+++
@@ -1,9 +0,0 @@
-package hu.unideb.inf.dandy.szd.jpa.entity;
-
-import javax.persistence.MappedSuperclass;
-
-@MappedSuperclass
-public enum Rank {
-
-	ADMIN, ORGANIZER, USER;
-}
---
+++ BreakDown-jpa/src/main/java/hu/unideb/inf/dandy/szd/jpa/entity/Role.java
@@ -0,0 +1,19 @@
+package hu.unideb.inf.dandy.szd.jpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name= "roles")
+public class Role extends BaseId{
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+}
