bb19d8fe87077039df44a284d5dbfb78f9d1cce2
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-22 14:45:07 +0100

    Added scripts to show modals automatically when needed.

--- BreakDown-web/src/main/webapp/resources/js/init.js
+++ BreakDown-web/src/main/webapp/resources/js/init.js
@@ -10,6 +10,25 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     $('select').material_select();
+    var url = window.location.href;
+    if(url.indexOf('?error') != -1) {
+    	$('#login_modal').modal({
+    		inDuration: 0
+    	});
+        $('#login_modal').modal('open');
+        $('#login_modal').modal({
+    		inDuration: 300
+    	});
+    }
+    if(url.indexOf('?regerror') != -1) {
+    	$('#reg_modal').modal({
+    		inDuration: 0
+    	});
+        $('#reg_modal').modal('open');
+        $('#reg_modal').modal({
+    		inDuration: 300
+    	});
+    }
   });
 
 $('.datepicker').pickadate({
@@ -21,3 +40,9 @@ $('.datepicker').pickadate({
     	  $('.picker').appendTo('body');
     	}
   });
+
+$('.dropdown-button').dropdown({
+	hover: true,
+	belowOrigin: true
+});
+
