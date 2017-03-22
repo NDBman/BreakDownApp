1a5fc8c7cfd4ebce5b12a3d6d50fd67987d7dcfd
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-22 14:44:26 +0100

    Implemented registartion controller with full feedback.

--- BreakDown-web/src/main/java/hu/unideb/inf/dandy/szd/web/controller/IndexController.java
+++ BreakDown-web/src/main/java/hu/unideb/inf/dandy/szd/web/controller/IndexController.java
@@ -1,14 +1,17 @@
 package hu.unideb.inf.dandy.szd.web.controller;
 
 import java.text.ParseException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import hu.unideb.inf.dandy.szd.services.BreakerServices;
+import hu.unideb.inf.dandy.szd.services.feedback.FeedbackCodes;
 
 @Controller
 @RequestMapping("/")
@@ -22,19 +25,43 @@ public class IndexController {
 		return "index";
 	}
 	
+	@RequestMapping(value="index")
+	public String indexToo(){
+		return "index";
+	}
+	
 	@RequestMapping(value="reg", method=RequestMethod.POST)
 	public String registrate(@RequestParam(name = "name") String name,
 			@RequestParam(name = "breaker_name") String bname, @RequestParam(name = "password") String password,
 			@RequestParam(name = "password_a") String passwordAgain, @RequestParam(name = "gender") String gender,
-			@RequestParam(name = "birthday") String birthday) throws NumberFormatException, ParseException{
-		System.out.println(birthday);
-		breakerServices.createBreaker(name, bname, password, passwordAgain, birthday, Long.parseLong(gender));
+			@RequestParam(name = "birthday") String birthday, ModelMap model) throws NumberFormatException, ParseException{
+		List<FeedbackCodes> feedbackCodes = breakerServices.createBreaker(name, bname, password, passwordAgain, birthday, Long.parseLong(gender));
 		
-		return "index";
+		if(feedbackCodes.contains(FeedbackCodes.PASS))
+			return "index";
+		
+		for(FeedbackCodes fbc : feedbackCodes){
+			if(fbc == FeedbackCodes.NOT_MATCHING_PASSWORDS)
+				model.addAttribute("password", "nomatch");
+			if(fbc == FeedbackCodes.USERNAME_ALREADY_EXIST)
+				model.addAttribute("username", "exists");
+			if(fbc == FeedbackCodes.NULL_NAME)
+				model.addAttribute("name", "null");
+			if(fbc == FeedbackCodes.NULL_BBOYNAME)
+				model.addAttribute("username", "null");
+			if(fbc == FeedbackCodes.NULL_PASSWORD)
+				model.addAttribute("password", "null");
+			if(fbc == FeedbackCodes.NULL_PASSWORD_AGAIN)
+				model.addAttribute("password_again", "null");
+			if(fbc == FeedbackCodes.NULL_BIRTHDAY)
+				model.addAttribute("birthday", "null");
+				
+		}
+		return "redirect:index?regerror";
 		
 	}
 	
-	@RequestMapping(value="login", method=RequestMethod.GET)
+	@RequestMapping(value="login", method=RequestMethod.POST)
 	public String gotoLogin(){
 		return "login";
 	}
