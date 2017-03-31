3e92083b6d485f1a616734a5190195f7b589cc1b
Nándor Szabo <n.szabo@bssn-software.de>
2017-03-31 20:04:57 +0200

    Deleted InternalResourceResolver bean.

--- BreakDown-web/src/main/java/hu/unideb/inf/dandy/szd/web/config/BreakDownAppConfig.java
+++ BreakDown-web/src/main/java/hu/unideb/inf/dandy/szd/web/config/BreakDownAppConfig.java
@@ -1,14 +1,11 @@
 package hu.unideb.inf.dandy.szd.web.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 import hu.unideb.inf.dandy.szd.jpa.config.BreakDownJpaModulConfig;
 import hu.unideb.inf.dandy.szd.services.config.BreakDownServiceModuleConfig;
@@ -20,19 +17,9 @@ import hu.unideb.inf.dandy.szd.services.config.BreakDownServiceModuleConfig;
 @Import({BreakDownServiceModuleConfig.class, BreakDownJpaModulConfig.class})
 public class BreakDownAppConfig extends WebMvcConfigurerAdapter{
 	
-	@Bean
-	public InternalResourceViewResolver viewResolver(){
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
-	}
-	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**")
-				.addResourceLocations("/resources/");
-		super.addResourceHandlers(registry);
+		registry.addResourceHandler("/**")
+				.addResourceLocations("/");
 	}
 }
