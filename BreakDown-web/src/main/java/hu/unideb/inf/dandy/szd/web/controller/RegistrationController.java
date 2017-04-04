7fd9a574dd4a0af1a704aedc7963533b49bfe546
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-04 10:54:42 +0200

    changed bboyname field to username.

--- BreakDown-web/src/main/java/hu/unideb/inf/dandy/szd/web/controller/RegistrationController.java
+++ BreakDown-web/src/main/java/hu/unideb/inf/dandy/szd/web/controller/RegistrationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import hu.unideb.inf.dandy.szd.jpa.entity.Breaker;
+import hu.unideb.inf.dandy.szd.jpa.entity.BreakerEntity;
 import hu.unideb.inf.dandy.szd.services.BreakerServices;
 import hu.unideb.inf.dandy.szd.services.GenderServices;
 
@@ -30,21 +30,21 @@ public class RegistrationController {
 
 	@RequestMapping(value="/reg", method=RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<Breaker> registrateBreaker(@RequestParam(required = true) String name, @RequestParam(required = true) String username,
+	public ResponseEntity<BreakerEntity> registrateBreaker(@RequestParam(required = true) String name, @RequestParam(required = true) String username,
 									@RequestParam(required = true) String email, @RequestParam(required = true) String password,
 									@RequestParam(required = true) String birthday, @RequestParam(required = true) Long gender) throws ParseException{
-		Breaker breaker = Breaker.builder()
+		BreakerEntity breaker = BreakerEntity.builder()
 						.name(name)
-						.bboyName(username)
+						.username(username)
 						.email(email)
 						.password(password)
 						.birthday(dateFormatter.parse(birthday, Locale.US))
 						.gender(genderServices.getGender(gender))
 						.build();
 		if(breakerServices.matchingEmails(breaker)){
-			return new ResponseEntity<Breaker>(breaker, HttpStatus.NOT_ACCEPTABLE);
+			return new ResponseEntity<BreakerEntity>(breaker, HttpStatus.NOT_ACCEPTABLE);
 		}
-		return new ResponseEntity<Breaker>(breakerServices.createBreaker(breaker),HttpStatus.OK);
+		return new ResponseEntity<BreakerEntity>(breakerServices.createBreaker(breaker),HttpStatus.OK);
 		
 	}
 }
