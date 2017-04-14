2a39a089d5aa1abf34bc65ecb5ca115817321627
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-14 17:46:54 +0200

    Adding travis ci file.

--- BreakDown-services/src/main/java/hu/unideb/inf/dandy/szd/services/feedback/FeedbackCodes.java
+++
@@ -1,8 +0,0 @@
-package hu.unideb.inf.dandy.szd.services.feedback;
-
-public enum FeedbackCodes {
-
-	NULL_NAME, NULL_BBOYNAME, NULL_PASSWORD, NULL_PASSWORD_AGAIN, NULL_BIRTHDAY, 
-	USERNAME_ALREADY_EXIST, NOT_MATCHING_PASSWORDS,
-	PASS;
-}
