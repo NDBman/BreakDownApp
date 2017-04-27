10c61f3b9e20f5b291ad4c47274a57dd1b0718a4
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-27 04:38:10 +0200

    Added auto close when menubutton is clicked.

--- BreakDown-web/src/main/webapp/resources/angularjs/controllers/sideNavController.js
+++ BreakDown-web/src/main/webapp/resources/angularjs/controllers/sideNavController.js
@@ -3,4 +3,8 @@ breakDownApp.controller("sideNavController", function($scope, $mdSidenav){
 		$mdSidenav('left')
 			.toggle();
 	}
+	$scope.close = function(){
+		$mdSidenav('left')
+			.close();
+	}
 })
